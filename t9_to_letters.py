a87f2421971bacfea89de0373c053bdabcec5dee
= <killikomfr@gmail.com>
2022-09-08 01:19:48 +0200

    Initialization

---
+++ t9_to_letters.py
@@ -0,0 +1,13 @@
+correspondance = [
+	["a", "b", "c"], ["d", "e", "f"],
+	["g", "h", "i"], ["j", "k", "l"], ["m", "n", "o"],
+	["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]
+]
+
+print(correspondance)
+
+def from_t9 (str) :
+	pass
+
+def to_t9 (str) :
+	pass
