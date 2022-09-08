9c8e866bf0b0fed6d2b0118a18d0139af9887467
= <killikomfr@gmail.com>
2022-09-08 02:19:56 +0200

    Added caps to function to_t9

--- t9_to_letters.py
+++ t9_to_letters.py
@@ -4,16 +4,15 @@ correspondance = [
 	["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]
 ]
 
-print(correspondance)
-
 def from_t9 (str) :
 	pass
 
-def to_t9 (str) :
+def to_t9 (input_str) :
+	input_str = input_str.lower()
 	splitted_str = []
 	result = ""
-	for l in str :
-		splitted_str.append(l)
+	for l in input_str :
+		splitted_input_str.append(l)
 		for i in range(len(correspondance)) :
 			if (l in correspondance[i]) :
 				for j in range(correspondance[i].index(l) + 1) :
