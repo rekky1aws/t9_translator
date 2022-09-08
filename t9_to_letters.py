0f5a9d78cc4e25c9b73cb6d23aab95eff3217007
= <killikomfr@gmail.com>
2022-09-08 02:16:36 +0200

    Added function to get T9 code from string
    Function to_t9 handles "abcdefghijklmnopqrstuvwxyz"

--- t9_to_letters.py
+++ t9_to_letters.py
@@ -10,4 +10,15 @@ def from_t9 (str) :
 	pass
 
 def to_t9 (str) :
+	splitted_str = []
+	result = ""
+	for l in str :
+		splitted_str.append(l)
+		for i in range(len(correspondance)) :
+			if (l in correspondance[i]) :
+				for j in range(correspondance[i].index(l) + 1) :
+					result = f"{result}{i + 2}"
+	return result
 	pass
+
+print("coucou", to_t9("coucou"))
