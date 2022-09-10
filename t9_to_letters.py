1f4b4fa0ba2db4680e11d77d87f52e519bb993f0
= <killikomfr@gmail.com>
2022-09-10 14:14:57 +0200

    Trying to implement function from_t9.

--- t9_to_letters.py
+++ t9_to_letters.py
@@ -4,7 +4,23 @@ correspondance = [
 	["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]
 ]
 
-def from_t9 (str) :
+def from_t9 (input_str) :
+	nb_p = 0
+	last_pressed = int(input_str[1]) - 2
+	print(last_pressed)
+	result = ""
+	for l in input_str :
+		if int(l) - 2 == last_pressed:
+			if nb_p <= len(correspondance[last_pressed]) :
+				nb_p += 1
+			else :
+				result += correspondance[last_pressed][nb_p - 1]
+				nb_p = 0
+		else :
+			result += correspondance[last_pressed][nb_p - 1]
+			nb_p = 0
+			last_pressed = int(l) - 2
+	return result
 	pass
 
 def to_t9 (input_str) :
@@ -20,3 +36,4 @@ def to_t9 (input_str) :
 	return result
 
 print("coucou", to_t9("coucou"))
+print(from_t9(to_t9("coucou")))
